a76fbc3b3273723a21221b001e4a6a37f9bd35aa
Vanessa Nakamura <vannynakamura06@gmail.com>
2023-01-31 18:51:11 -0300

    att

--- src/components/podcast/Podcast.jsx
+++ src/components/podcast/Podcast.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import css from "./Podcast.module.css";
-import LogoFooter from "../../assets/LogoFooter.png";
+import LogoAzul from "../../assets/logoazul.png";
+import { AiFillPlayCircle } from "react-icons/ai";
 
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  let element = document.getElementById("embed-iframe");
+  let options = {
+    uri: "spotify:episode:7makk4oTQel546B0PZlDM5",
+  };
+  let callback = (EmbedController) => {};
+  IFrameAPI.createController(element, options, callback);
+};
 const About = () => {
   return (
     <div className={css.container}>
@@ -11,24 +20,32 @@ const About = () => {
       <div className={css.linha}>
         <hr align="center" />
       </div>
-      <div className={css.wrapper}>
-        <div className={css.circle}></div>
-        <img src={LogoFooter} alt="" width={600} />
-      </div>
-      <div className={css.wrapper}>
-        <div className={css.circle}></div>
-        <img src={LogoFooter} alt="" width={600} />
-      </div>
-      <div className={css.wrapper}>
-        <div className={css.circle}></div>
-        <img src={LogoFooter} alt="" width={600} />
-      </div>
-      <div className={css.wrapper}>
-        <div className={css.circle}></div>
-        <img src={LogoFooter} alt="" width={600} />
+      <div className={css.wrapperCard}>
+        <div className={css.card}>
+          <div className={css.cardImage}>
+            <img src={LogoAzul} alt="" width={600} />
+          </div>
+        </div>
+        <div className={css.cardContent}>
+          <h3>Titulo do Podcast</h3>
+        </div>
+        <div className={css.descricao}>
+          <p>
+            Lorem ipsum dolor sit amet. Ut beatae fugiat qui velit ipsa ut
+            delectus veritatis ea earum molestiae aut iusto labore et laboriosam
+            commodi. Sed error rerum et minima quod non beatae quia. In
+            recusandae galisum ea itaque optio ab sunt Quis ut odio doloribus
+            est debitis laboriosam.
+          </p>
+        </div>
+        <div className={css.data}>
+          <span>dez. de 2022 - 54 min 3 s</span>
+        </div>
+        <div className={css.play}>
+          <AiFillPlayCircle className={css.spot} />
+        </div>
       </div>
     </div>
   );
 };
-
 export default About;
